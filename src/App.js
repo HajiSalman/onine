ecc1943432980c9e8d7cb38cbf93d5f55556ed9b
HajiSalman <faridz4life@gmail.com>
2022-09-01 09:14:48 +0700

    Init

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import NavigationBar from './components/NavigationBar';
+import SearchBar from './components/SearchBar';
+import Landing from './components/Landing';
+import Isi from './components/Isi';
+import Testi from './components/Testi';
+import "./style/landingPage.css"
+function App() {
+  return (
+    <div > 
+      <div >
+        
+      <SearchBar/>
+      <NavigationBar/>
+      <Landing/>
+      <Isi/>
+      <Testi/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
